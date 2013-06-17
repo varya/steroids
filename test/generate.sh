fe7cfbfc933238df99797c280fd9e6afe7cbcad4
Harri Sarsa <harri.sarsa@appgyver.com>
2013-06-17 12:45:37 +0300

    fixed generation of all examples app

--- test/generate.sh
+++ test/generate.sh
@@ -7,33 +7,40 @@ rm -rf testApp
 steroids create testApp
 cd testApp
 
-CORDOVA_EXAMPLES="accelerometer audio camera compass device geolocation"
+CORDOVA_EXAMPLES="accelerometer audio camera compass device geolocation notification storage"
 STEROIDS_EXAMPLES="animation drawer drumMachine layerStack modal navigationBar photoGallery preload"
 
-rm -rf config/application.coffee
-cp ../application.coffee.head config/application.coffee
+rm -rf www/index.html
+cp ../test/index.html.head www/cordovaIndex.html
+cp ../test/index.html.head www/steroidsIndex.html
 
+echo "    <h3 class='topcoat_list__header'>Cordova Examples</h2>" >> www/cordovaIndex.html
+echo "    <ul class='topcoat-list'>" >> www/cordovaIndex.html
 
 for CORDOVA_EXAMPLE in $CORDOVA_EXAMPLES; do
-	steroids generate example $CORDOVA_EXAMPLE
+  steroids generate example $CORDOVA_EXAMPLE
 
-  echo "{" >> config/application.coffee
-  echo "  title: '$CORDOVA_EXAMPLE'" >> config/application.coffee
-  echo "  icon: ''" >> config/application.coffee
-  echo "  location: '"$CORDOVA_EXAMPLE"Example.html'" >> config/application.coffee
-  echo "}"  >> config/application.coffee
+  echo "      <li class='topcoat-list__item' data-location='"$CORDOVA_EXAMPLE"Example.html'>" >> www/cordovaIndex.html
+  echo "       "$CORDOVA_EXAMPLE >> www/cordovaIndex.html
+  echo "      </li>" >> www/cordovaIndex.html
 done
+
+echo "    <h3 class='topcoat_list__header'>Steroids Examples</h2>" >> www/steroidsIndex.html
+echo "    <ul class='topcoat-list'>" >> www/steroidsIndex.html
 
 for STEROIDS_EXAMPLE in $STEROIDS_EXAMPLES; do
   steroids generate example $STEROIDS_EXAMPLE
 
-  echo "{" >> config/application.coffee
-  echo "  title: '$STEROIDS_EXAMPLE'" >> config/application.coffee
-  echo "  icon: ''" >> config/application.coffee
-  echo "  location: 'http://localhost/views/"$STEROIDS_EXAMPLE"Example/index.html'" >> config/application.coffee
-  echo "}"  >> config/application.coffee
+  echo "      <li class='topcoat-list__item' data-location='http://localhost/views/"$STEROIDS_EXAMPLE"Example/index.html'>" >> www/steroidsIndex.html
+  echo "       "$STEROIDS_EXAMPLE >> www/steroidsIndex.html
+  echo "      </li>" >> www/steroidsIndex.html
 done
 
-cat ../application.coffee.tail >> config/application.coffee
+cat ../test/index.html.tail >> www/steroidsIndex.html
+cat ../test/index.html.tail >> www/steroidsIndex.html
+
+rm -rf config/application.coffee
+cp ../test/application.coffee config/application.coffee
+
 
 steroids connect
