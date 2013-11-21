82898492afe9ea502bddacca039d6b32620da742
Harri Sarsa <harri.sarsa@appgyver.com>
2013-11-21 17:53:20 +0200

    Gruntfile.js and package.json defaults bettered

--- templates/applications/default/Gruntfile.js
+++ templates/applications/default/Gruntfile.js
@@ -9,13 +9,6 @@
 
 module.exports = function(grunt) {
 
-  // Load all Grunt task modules in the project first, except for grunt-steroids
-  require('load-grunt-tasks')(grunt, {pattern: ['grunt-*', '!grunt-steroids']});
-
-  // User configs go here
-  grunt.initConfig();
-
-  // grunt-steroids must be loaded separately after grunt.initConfig()
   grunt.loadNpmTasks("grunt-steroids");
 
   grunt.registerTask("default", ["steroids-make", "steroids-compile-sass"]);
