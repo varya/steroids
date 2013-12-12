0a97a89cc23b5c9be26175d150fa9796007a3a2f
Harri Sarsa <harri.sarsa@appgyver.com>
2013-12-12 17:30:26 +0200

    removed steroids.js updating

--- publish-plz.sh
+++ publish-plz.sh
@@ -2,26 +2,16 @@
 
 head -n 20 CHANGELOG.md
 
-echo "is changelog ok?"
+echo "Is changelog ok?"
 read
-
-echo "updating template projects to latest version with steroids-js"
-
-sh update-steroidsjs-templates.sh
-
-echo "ok?"
-read
-
-git add templates
-git commit -m "updated templates to use latest steroids-js"
 
 git status
 
-echo "ok?"
+echo "Git status ok?"
 
 read
 
-echo "versioning the npm, now it's a good time to control+c if you don't want to patch ?"
+echo "Versioning the npm, now it's a good time to CTRL+C if you don't want to patch?"
 
 read
 
